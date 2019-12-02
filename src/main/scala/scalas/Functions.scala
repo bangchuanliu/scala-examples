f61d48510b84b1e6d051629629939bfeee9ca6b7
banliu <banliu@linkedin.com>
2019-12-01 23:29:24 -0800

    add classes

--- src/main/scala/scalas/Functions.scala
+++ src/main/scala/scalas/Functions.scala
@@ -2,12 +2,19 @@ package scalas
 
 object Functions {
 
+  // implicit return types
+  val toUpperCase2 = (s: String) => s.toUpperCase
+
+  // explicit return types
   val toUpperCase: String => String = _.toUpperCase
   val append: (String, String) => String = _ + _
-  val toUpperCaseThenAppend = toUpperCase.andThen(append(_, "@gmail.com")) 
-  
+//  val toUpperCaseThenAppend = toUpperCase.andThen(append(_, "@gmail.com"))
+
+  val double = (i : Int) => {i * 2}
   
   def main(args: Array[String]): Unit = {
-    println(toUpperCaseThenAppend("kevin"))
+//    println(toUpperCaseThenAppend("kevin"))
+
+    println(double(2))
   }
 }
