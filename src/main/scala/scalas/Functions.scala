1df3c847270e7a6a4cc09809f190b68eef7b8f5e
banliu <banliu@linkedin.com>
2019-12-02 02:08:29 -0800

    add functions

--- src/main/scala/scalas/Functions.scala
+++ src/main/scala/scalas/Functions.scala
@@ -16,5 +16,35 @@ object Functions {
 //    println(toUpperCaseThenAppend("kevin"))
 
     println(double(2))
+
+    exec(sayHello, "kevin")
+
+    execClosure(Closures.isVotingAge, 21)
+    execClosure(Closures.isVotingAge, 16)
+    Closures.votingAge = 15
+    execClosure(Closures.isVotingAge, 16)
+
+    val sayHi = saySomething("HI")
+    println(sayHi("AI"))
+  }
+
+
+  def exec(f: String => Unit,  name : String): Unit = {
+    f(name)
+  }
+
+  val hello = "hello"
+
+  def sayHello(name : String): Unit = {
+    println(s"$hello, $name")
+  }
+
+  def execClosure(f: Int => Boolean, age: Int): Unit = {
+    println(f(age))
+  }
+
+  // function return a function
+  def saySomething(prefix: String): String => String = {
+    (s : String) => prefix + " " + s
   }
 }
