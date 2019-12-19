663d09aa852f12affca840c6b946fdbe762fff52
banliu <banliu@linkedin.com>
2019-12-18 19:34:07 -0800

    add nth

---
+++ src/main/fpinscala/week3/list/nth.scala
@@ -0,0 +1,10 @@
+package week3.list
+
+object nth {
+
+  def nth[T](n: Int, xs: List[T]): T = {
+    if (xs.isEmpty) throw new IndexOutOfBoundsException
+    else if (n == 0) xs.head
+    else nth(n - 1, xs.tail)
+  }
+}
