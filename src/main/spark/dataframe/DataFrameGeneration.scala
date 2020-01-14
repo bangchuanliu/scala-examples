6d2a75307d43a92f1a3880a39e0e00809fbc3ca8
liubangchuan <liubangchuan@liubanghuansMBP.attlocal.net>
2020-01-13 21:05:58 -0800

    add df generation

---
+++ src/main/spark/dataframe/DataFrameGeneration.scala
@@ -0,0 +1,58 @@
+package dataframe
+
+import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+
+object DataFrameGeneration extends AbstractSpark {
+
+  import spark.implicits._
+
+  val columns = List("name", "age")
+  val data = {
+    def createList(n: Int): List[(String, Int)] = if (n == 0) Nil else ("name-" + n, n) :: createList(n - 1)
+    createList(3)
+  }
+  val rdd = spark.sparkContext.parallelize(data)
+
+
+  // create DataFrame from RDD
+  val dfFromRDD1 = rdd.toDF()
+  val dfFromRDD2 = rdd.toDF("name", "age")
+  val dfFromRDD3 = rdd.toDF(columns: _*)
+  val dfFromRDD4 = spark.createDataFrame(rdd).toDF(columns: _*)
+
+  val schema : StructType = StructType(Array(
+    StructField("name", StringType, nullable = true),
+    StructField("age", IntegerType, nullable = true)
+  ))
+  val rowRdd = rdd.map(r => Row(r._1, r._2))
+  val dfFromRDD5 = spark.createDataFrame(rowRdd, schema)
+
+  // create DataFrame from collections
+  val dfFromCollection1 = data.toDF()
+  val dfFromCollection2 = data.toDF(columns:_*)
+  val dfFromCollection3 = spark.createDataFrame(data).toDF(columns: _*)
+  val rowData : List[Row] = data.map(d => Row(d._1, d._2))
+//  val dfFromCollection4 = spark.createDataFrame(rowData, schema)
+  
+  // create DataFrame from structure data
+  lazy val dfFromText = spark.read.json("/src/main/resources/file.txt")
+  lazy val dfFromCSV = spark.read.csv("/src/main/resources/file.csv")
+  lazy val dfFromJson = spark.read.json("/src/main/resources/file.json")
+  
+  // create DataFrame from Hive
+  // create DataFrame from RDBMS
+  // create DataFrame from Hbase
+  // create DataFrame from Kafka
+
+  // create DataFrame from Avro
+  def df2Avro() = {
+    val avro = spark.read.format("avro").load("/src/main/resources/file.avro")
+  }
+  
+  def main(args: Array[String]): Unit = {
+    
+  }
+
+
+}
--- src/main/spark/dataframe/SparkDateFrame.scala
+++ src/main/spark/dataframe/SparkDateFrame.scala
@@ -1,28 +1,57 @@
 package dataframe
 
+import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.types.{DataType, IntegerType, StringType, StructField, StructType}
+
+import scala.collection.mutable.ListBuffer
+
 object SparkDateFrame extends AbstractSpark {
-  
+
   def main(args: Array[String]): Unit = {
+    createDataFrame
+  }
+  
+  
+  def highOrderFunction(): Unit = {
     import spark.implicits._
-    
+
     spark.range(2).toDF().show
-    
+
     println(spark.range(5).toDF.schema)
-    
-    val d1 = Seq(1,2,3,4,5).toDF()
-    val d2 = Seq(1,3,5, 7).toDF()
-    
+
+    val d1 = Seq(1, 2, 3, 4, 5).toDF()
+    val d2 = Seq(1, 3, 5, 7).toDF()
+
     // except
     d1.except(d2).show()
-    
+
     // union
     d1.union(d2).show()
 
     // union and dedupe
     d1.union(d2).distinct().show()
-    
+
     // left anti
     d1.join(d2, Seq("value"), "left_anti").show()
   }
-  
+
+  case class Person(name: String, age: Int)
+
+  def createDataFrame(): Unit = {
+    import spark.implicits._
+    var persons = ListBuffer[Person]()
+    for (i <- 1 to 4) {
+      val p = Person("a" + i, i)
+      persons += p
+    }
+    val schema = StructType(Array(
+      StructField("name", StringType, nullable = true),
+      StructField("age", IntegerType, nullable = true)
+    ))
+    val row = Row(persons.toList)
+    val sc = spark.sparkContext
+    val df = spark.createDataFrame(sc.parallelize(Array(row)), schema)
+    df.show()
+  }
+
 }
