37b84f016ea4f91b052020d442266b06e834fc39
liubangchuan <liubangchuan@liubanghuansMBP.attlocal.net>
2021-01-02 23:05:06 -0800

    Add prediction

--- spark/src/main/spark/ml/DecisionTree.scala
+++ spark/src/main/spark/ml/DecisionTree.scala
@@ -1,17 +1,45 @@
 package ml
 
+import org.apache.parquet.it.unimi.dsi.fastutil.Arrays
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{Binarizer, VectorAssembler}
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.sql.functions.col
 import utils.SparkUtil.spark
 
 object DecisionTree {
-  
+
   def main(args: Array[String]): Unit = {
-    decisionTree
+//    decisionTree
+//        predictions
+    minuteWeather
   }
 
-  def decisionTree():Unit = {
+  def minuteWeather():Unit = {
+    import spark.implicits._
+    
+    val df = spark.read.format("org.apache.spark.csv").option("header", true).option("inferSchema", true).csv("/Users/liubangchuan/data/coursea-bigdata/big-data-4/minute_weather.csv")
+    val filterDF = df.filter($"rowID" % 10 === 0)
+    df.describe()
+    
+    val rainAccumulation = filterDF.filter($"rain_accumulation" === 0.0)
+    val rainDuration = filterDF.filter($"rain_duration" === 0.0)
+    
+    val workingDF = filterDF.drop("rain_accumulation", "rain_duration", "hpwren_timestamp").na.drop()
+  }
+
+  def predictions(): Unit = {
+    val df = spark.read.format("org.apache.spark.csv").option("header", true).option("inferSchema", true).csv("/Users/liubangchuan/data/coursea-bigdata/predictions.csv")
+    val evaluator = new MulticlassClassificationEvaluator().setLabelCol("label").setPredictionCol("prediction").setMetricName("accuracy")
+    val accuracy = evaluator.evaluate(df)
+    println(s"Accuracy = ${accuracy}")
+    val metrics = new MulticlassMetrics(df.rdd.map(row => Tuple2(row.getDouble(0), row.getDouble(1))))
+    println(metrics.confusionMatrix.toArray.mkString(","))
+  }
+
+  def decisionTree(): Unit = {
     val df = spark.read.format("org.apache.spark.csv").option("header", true).option("inferSchema", true).csv("/Users/liubangchuan/data/coursea-bigdata/daily_weather.csv")
     val df2 = df.drop("number").na.drop()
     val binarizerDF = new Binarizer().setThreshold(24.99999).setInputCol("relative_humidity_3pm").setOutputCol("label").transform(df2)
@@ -28,6 +56,11 @@ object DecisionTree {
     val model = pipeline.fit(trainingData)
     val predictions = model.transform(testData)
 
-    predictions.select("prediction", "label").show(20)
+//    predictions.select("prediction", "label").show(20)
+    predictions.select("prediction", "label").write
+      .mode("overwrite")
+      .format("com.databricks.spark.csv")
+      .option("header", true)
+      .save("/Users/liubangchuan/data/coursea-bigdata/predictions.csv")
   }
 }
